529a23345238e52870cf1e36420fba344e69d6b4
Piotr Witek <piotrek.witek@gmail.com>
2017-08-17 00:01:39 +0200

    added user model example

---
+++ examples/src/models/user.ts
@@ -0,0 +1,50 @@
+export interface IUserDTO {
+  id: string;
+  first_name: string;
+  last_name: string;
+}
+
+export interface IUser {
+  constructor: {
+    fromDTO(user: IUserDTO): IUser;
+  }
+
+  id: string;
+  firstName: string;
+  lastName: string;
+  getFullName(): string;
+
+  toDTO(): IUserDTO;
+}
+
+export class User implements IUser {
+  'constructor': typeof User;
+
+  id: string;
+  firstName: string = 'Default';
+  lastName: string = 'Name';
+
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  static fromDTO(dto: IUserDTO): IUser {
+    const model = new User(dto.id);
+    model.firstName = dto.first_name;
+    model.lastName = dto.last_name;
+
+    return model;
+  }
+
+  getFullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  toDTO(): IUserDTO {
+    return {
+      id: this.id,
+      first_name: this.firstName,
+      last_name: this.lastName,
+    }
+  }
+}
