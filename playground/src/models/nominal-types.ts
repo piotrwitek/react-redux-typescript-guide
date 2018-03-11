2329d3a76088cf39cf3976855a0fdca53d6bbf5d
Piotr Witek <piotrek.witek@gmail.com>
2018-03-11 17:27:08 +0100

    added nominal types examples

---
+++ playground/src/models/nominal-types.ts
@@ -0,0 +1,41 @@
+// Nominal Typing
+// Usefull to model domain concepts that are using primitive data type for it's value
+
+// Method 1: using "interface"
+interface Name extends String { _brand: 'Name'; }
+const createName = (name: string): Name => {
+  // validation of business rules
+  return name as any;
+};
+
+// Method 2: using "enum"
+enum SurnameBrand { }
+type Surname = SurnameBrand & string;
+// interface Surname extends String { _brand: 'Surname'; }
+const createSurname = (surname: string): Surname => {
+  // validation of business rules
+  return surname as any;
+};
+
+type Person = {
+  name: Name;
+  surname: Surname;
+};
+
+const person: Person = {
+  name: createName('Piotr'),
+  surname: createSurname('Witek'),
+};
+
+// Type system will ensure that the domain objects can only contain correct data
+// person.name = 'Karol'; // error
+// person.name = person.surname; // error
+person.name = createName('Karol'); // OK!
+// person.surname = 'Mate'; // error
+// person.surname = person.name; // error
+person.surname = createSurname('Mate'); // OK!
+
+// easy casting to supertype
+let str: string;
+str = person.name.toString();
+str = person.surname;
