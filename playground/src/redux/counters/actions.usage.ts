d5849b8c5f0ee2b6350685abc917524bfefe1f04
Piotr Witek <piotrek.witek@gmail.com>
2018-04-03 19:54:43 +0200

    updated source file

--- playground/src/redux/counters/actions.usage.ts
+++ playground/src/redux/counters/actions.usage.ts
@@ -1,5 +1,5 @@
 import store from '@src/store';
 import { countersActions } from '@src/redux/counters';
 
-// store.dispatch(actionCreators.increment(1)); // Error: Expected 0 arguments, but got 1.
+// store.dispatch(countersActions.increment(1)); // Error: Expected 0 arguments, but got 1.
 store.dispatch(countersActions.increment()); // OK => { type: "INCREMENT" }
