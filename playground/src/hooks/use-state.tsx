c9c581ebd22af8933283880a69685c39014cc764
Sosuke Suzuki <aosukeke@gmail.com>
2019-01-27 03:52:08 +0900

    Add the example for useState to playground

---
+++ playground/src/hooks/use-state.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+interface CounterProps {
+  initialCount: number;
+}
+
+export default function Counter({initialCount}: CounterProps) {
+  const [count, setCount] = React.useState<number>(initialCount);
+  return (
+    <>
+      Count: {count}
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+    </>
+  );
+}
