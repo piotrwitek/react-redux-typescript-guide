5c31ec45434d3655bf329dcdf433c169fc037e9d
Mathias Soeholm <mathiassoeholm@gmail.com>
2019-04-25 11:41:29 +0200

    Is clean is now an sh script

---
+++ is-clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if
+    output=$(git status --porcelain) && [ -z "$output" ];
+then
+    echo 'Success!';
+else
+    (echo '>>> Please check CONTRIBUTING.md to learn how to properly amend README.md <<<' && false);
+fi
