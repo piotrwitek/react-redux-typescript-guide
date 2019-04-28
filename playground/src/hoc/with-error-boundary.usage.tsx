f402ba63327cea07b863124cf372007f82f2f89b
Mathias <mathiassoeholm@gmail.com>
2019-04-28 23:40:58 +0200

    Error boundary example fix (#166)
    * Updated error boundary example with working code
    
    * Updates based on review
    
    * Updated example in readme
    
    * Some white space change

--- playground/src/hoc/with-error-boundary.usage.tsx
+++ playground/src/hoc/with-error-boundary.usage.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 
 import { withErrorBoundary } from '../hoc';
 import { ErrorMessage } from '../components';
@@ -6,11 +6,29 @@ import { ErrorMessage } from '../components';
 const ErrorMessageWithErrorBoundary =
   withErrorBoundary(ErrorMessage);
 
-const BrokenButton = () => (
-  <button type="button" onClick={() => { throw new Error(`Catch me!`); }}>
-    {`Throw nasty error`}
-  </button >
-);
+const BrokenComponent = () => {
+  throw new Error('I\'m broken! Don\'t render me.');
+};
+
+const BrokenButton = () => {
+  const [shouldRenderBrokenComponent, setShouldRenderBrokenComponent] =
+    useState(false);
+
+  if (shouldRenderBrokenComponent) {
+    return <BrokenComponent />;
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        setShouldRenderBrokenComponent(true);
+      }}
+    >
+      {`Throw nasty error`}
+    </button>
+  );
+};
 
 export default () => (
   <ErrorMessageWithErrorBoundary>
